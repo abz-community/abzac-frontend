059ec03c6230fad156daa3fb0f29121e5b164065
yzned <harlydeaz@gmail.com>
2025-09-14 20:43:17 +0600

    delete prev

--- src/routes/profile/quotes.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/profile/quotes')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/profile/quotes"!</div>
-}
--- src/routes/profile/upload.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/profile/upload')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/profile/upload"!</div>
-}
